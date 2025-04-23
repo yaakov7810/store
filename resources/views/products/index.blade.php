b5d45ccf99403858387511d47dbe17a11e58301b
yaakov7810 <yaakov7810@gmail.com>
2025-04-23 13:54:16 +0300

    Delete resources/views/products directory

--- resources/views/products/index.blade.php
+++
@@ -1,6 +0,0 @@
-<h1>כל המוצרים</h1>
-<ul>
-@foreach ($products as $product)
-    <li><a href="{{ url('/products/' . $product->id) }}">{{ $product->name }}</a></li>
-@endforeach
-</ul>
--- resources/views/products/show.blade.php
+++
@@ -1,8 +0,0 @@
-<h1>{{ $product->name }}</h1>
-<p>{{ $product->description }}</p>
-@if ($product->image)
-    <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->name }}">
-@endif
-@if ($product->file)
-    <p><a href="{{ asset('storage/' . $product->file) }}">הורד את הקובץ</a></p>
-@endif
