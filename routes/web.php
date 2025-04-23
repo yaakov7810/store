e085a3e5afb1201414529d1364ba6a234aab9fe6
yaakov7810 <yaakov7810@gmail.com>
2025-04-23 13:54:31 +0300

    Delete routes directory

--- routes/web.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-
-Route::get('/products', [ProductController::class, 'index']);
-Route::get('/products/{product}', [ProductController::class, 'show']);
